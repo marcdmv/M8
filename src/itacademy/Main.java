a474df5b35f8d982c9fd3aff68d060b088181c56
Marc de Mas <marcdmv@gmail.com>
2020-10-14 11:55:33 +0200

    Comentarios añadidos

--- src/itacademy/Main.java
+++ src/itacademy/Main.java
@@ -6,47 +6,59 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        // Creamos los dos objetos de tipo Rocket
         Rocket rocket1 = new Rocket();
         Rocket rocket2 = new Rocket();
 
+        // Creamos una lista de propulsores por cohete
         List<Propeller> rocket1Propellers;
         List<Propeller> rocket2Propellers;
 
+        // Definimos manualmente las poténcias máximas de cada propulsor de ambos cohetes
         double[] rocket1PropellersPower = new double[]{10,30,80};
         double[] rocket2PropellersPower = new double[]{30,40,50,50,30,10};
 
+        // Utilizamos el método setCode para asignar un nombre a cada cohete
         rocket1.setCode("32WESSDS");
         rocket2.setCode("LDSFJA32");
 
+        // Asignamos la potencia máxima a cada propulsor utilizando el método setMaxPowerPropellers
         rocket1Propellers = setMaxPowerPropellers(rocket1PropellersPower);
         rocket2Propellers = setMaxPowerPropellers(rocket2PropellersPower);
 
-        System.out.println("FASE 1");
+        // FASE 1
+        // Mostramos por consola cuántos propulsores tiene cada cohete
         System.out.println("El cohete " + rocket1.getCode() + " tiene " + rocket1Propellers.size() + " propulsores.");
         System.out.println("El cohete " + rocket2.getCode() + " tiene " + rocket2Propellers.size() + " propulsores.");
 
-        System.out.println("FASE 2");
-        int i = 1;
-        System.out.println("Los propulsores del cohete " + rocket1.getCode() + " tienen las potencias máximas siguientes:");
-        for (Propeller p : rocket1Propellers) {
-            System.out.println("Rocket " + rocket1.getCode() + " - Propeller #" + i + ": " + p.getMaxPower());
-            i++;
-        }
-        i = 1;
-        System.out.println("Los propulsores del cohete " + rocket2.getCode() + " tienen las potencias máximas siguientes:");
-        for (Propeller p : rocket2Propellers) {
-            System.out.println("Rocket " + rocket2.getCode() + " - Propeller #" + i + ": " + p.getMaxPower());
-            i++;
-        }
+        // FASE 2
+        // Mostramos por consola la potencia máxima de cada propulsor usando el método getter getMaxPower() para cada propulsor
+        showPropellersMaxPower (rocket1, rocket1Propellers);
+        showPropellersMaxPower (rocket2, rocket2Propellers);
 
+        // FASE 3 y 4
+        // Definimos manualmente la potencia objetivo de cada propulsor.
         double[] rocket1PropellersTargetPower = new double[]{5,25,75};
         double[] rocket2PropellersTargetPower = new double[]{25,35,45,45,20,5};
-        double v0 = 0;
-        double v1 = 500;
+        // Definimos las velocidades inicial y final de los cohetes. Para simplificar, se asignan para ambos cohetes, pero se podrían asignar velocidades distintas para cada cohete.
+        // v1 = v0 + 100 * sqrt(PotenciaTotal)
+        // PotenciaTotal = ((v1 - v0)/100)^2
+        double v0 = 150;
+        double v1 = 500; // Es importante remarcar que la fórmula define la PotentiaTotal necesaria siempre positiva, aunque el cohete frene. Por este motivo no se permite una v0 > v1
+        // Montamos y ejecutamos los hilos. Uno para cada cohete.
         java.lang.Runnable r1 = new RocketThreads(rocket1, rocket1Propellers, rocket1PropellersTargetPower, v0, v1);
         java.lang.Runnable r2 = new RocketThreads(rocket2, rocket2Propellers, rocket2PropellersTargetPower, v0, v1);
         new Thread(r1).start();
         new Thread(r2).start();
+    }
+
+    public static void showPropellersMaxPower (Rocket rocket, List<Propeller> propellers) {
+        int i = 1;
+        System.out.println("Los propulsores del cohete " + rocket.getCode() + " tienen las potencias máximas siguientes:");
+        for (Propeller p : propellers) {
+            System.out.println("Rocket " + rocket.getCode() + " - Propeller #" + i + ": " + p.getMaxPower());
+            i++;
+        }
     }
 
     public static List<Propeller> setMaxPowerPropellers (double[] rocketPropellersPower) {
@@ -102,7 +114,7 @@ class RocketThreads implements java.lang.Runnable {
         double requiredTotalPower;
 
         if (v0 > v1) {
-            System.out.println("La velocidad objetivo debe ser superior a la inicial");
+            System.out.println("La velocidad final debe ser superior a la inicial");
             System.exit(1);
         }
 
